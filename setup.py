45ec6dd909ee31dd261f110320717fd4ee384f38
Maciej Ciemborowicz <maciej.ciemborowicz@gmail.com>
2025-12-22 12:31:07 +0100

    Fix deb systemd unit install path

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="spotify-cmd",
-    version="0.1.13",
+    version="0.1.14",
     packages=find_packages(),
     install_requires=[
         'argparse==1.4.0',
